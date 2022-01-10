dce9c59fbd90c0f67cf395dc1e353a6f3ac7d0be
VABIII <tbengtson82@gmail.com>
2022-01-10 14:56:13 -0600

    post endpoint working

--- api/server.js
+++ api/server.js
@@ -7,13 +7,38 @@ server.use(express.json())
 
 const Users = require('./users/model')
 
+server.post('/api/users', async (req, res) => {
+    const { name, bio } = req.body
+    try {
+        if(!name || !bio) {
+            res.status(400).json({
+                message: "Please provide name and bio for the user"
+            })
+        } else {
+            const newUser = Users.insert({name, bio})
+            res.status(201).json(newUser)
+        }
+
+    }
+    catch (err) {
+        res.status(500).json({
+            message: "There was an error while saving the user to the database",
+            error: err.message
+        })
+    }
+})
+
+
 server.get("/api/users", async (req, res) => {
     try {
         const users = await Users.find()
         res.json(users)
     }
     catch (err) {
-        res.status(500).json({ message: err.message })
+        res.status(500).json({
+            message: 'The users information could not be retrieved',
+            error: err.message,
+        })
     }
 })
 
