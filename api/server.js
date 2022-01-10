cae34ce4f3fd20afca40b1b5c67149a1c53092bc
VABIII <tbengtson82@gmail.com>
2022-01-10 14:02:28 -0600

    first endpoint returning all users

--- api/server.js
+++ api/server.js
@@ -1,3 +1,50 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json())
+
+const Users = require('./users/model')
+
+server.get("/api/users", async (req, res) => {
+    try {
+        const users = await Users.find()
+        res.json(users)
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
